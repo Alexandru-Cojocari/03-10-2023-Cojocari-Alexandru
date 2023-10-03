ae36cd2c5d2ba3692e1c389be9d25c74c9866a2f
Alexandru-Cojocari <144117735+Alexandru-Cojocari@users.noreply.github.com>
2023-10-03 21:33:20 +0300

    Add files via upload

---
+++ Exercitiul 7 Cojocari Alexandru.py
@@ -0,0 +1,5 @@
+v=int(input("Dati varsta copilului:"))
+greutate_ideala = 2*v+8
+inaltime_ideala = 5*v+80
+print("Greutatea ideala pentru un copil de",v, "ani este", greutate_ideala, "kg.")
+print("Inaltimea ideala pentru un copil de",v, "ani este", inaltime_ideala, "cm.")

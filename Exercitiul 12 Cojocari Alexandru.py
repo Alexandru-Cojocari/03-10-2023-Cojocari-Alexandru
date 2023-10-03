ae9289d6b26c4c273884c592eecfca4d290566f1
Alexandru-Cojocari <144117735+Alexandru-Cojocari@users.noreply.github.com>
2023-10-03 21:43:30 +0300

    Add files via upload

---
+++ Exercitiul 12 Cojocari Alexandru.py
@@ -0,0 +1,6 @@
+a=int(input('Numarul de oua dat de prima gaina intro zi:'))
+b=int(input('Numarul de oua dat de a doua gaina intro zi:'))
+c=int(input('Numarul de oua dat de a treia gaina intro zi:'))
+d=int(input('Numarul de oua dat de a patra gaina intro zi:'))
+oua_pe_saptamana=(a+b+c+d)*7
+print('Într-o săptămână, cele 4 găini vor produce un total de',oua_pe_saptamana,' ouă.')

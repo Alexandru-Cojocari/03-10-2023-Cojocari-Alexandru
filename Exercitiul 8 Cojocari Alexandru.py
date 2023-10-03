ef7211ca8d3a9ca27a8023652d60791bf89e3ddc
Alexandru-Cojocari <144117735+Alexandru-Cojocari@users.noreply.github.com>
2023-10-03 21:38:36 +0300

    Add files via upload

---
+++ Exercitiul 8 Cojocari Alexandru.py
@@ -0,0 +1,9 @@
+cifra1=input("Dati prima cifra:")
+cifra2=input("Dati a doua cifra:")
+cifra3=input("Dati a treia cifra:")
+
+print(cifra1 + cifra2 + cifra3)
+print(cifra1 + cifra3 + cifra2)
+print(cifra2 + cifra1 + cifra3)
+print(cifra2 + cifra3 + cifra1)
+print(cifra3 + cifra1 + cifra2)

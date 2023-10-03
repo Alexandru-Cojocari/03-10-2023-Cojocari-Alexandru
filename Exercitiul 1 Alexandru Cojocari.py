92887fd068f1fd8a08ed8bf68f7ec5b0df505681
Alexandru-Cojocari <144117735+Alexandru-Cojocari@users.noreply.github.com>
2023-10-03 21:27:31 +0300

    Add files via upload

---
+++ Exercitiul 1 Alexandru Cojocari.py
@@ -0,0 +1,18 @@
+numar_rand = int(input('Dati numarul de rand al culorii curcubeului:'))
+
+if numar_rand == 1:
+ print('Culoarea cu numarul de rand 1 este rosu.')
+elif numar_rand == 2:
+ print('Culoarea cu numarul de rand 2 este portocaliu.')
+elif numar_rand == 3:
+ print('Culoarea cu numarul de rand 3 este galben.')
+elif numar_rand == 4:
+ print('Culoarea cu numarul de rand 4 este verde.')
+elif numar_rand == 5:
+ print('Culoarea cu numarul de rand 5 este albastru.')
+elif numar_rand == 6:
+ print('Culoarea cu numarul de rand 6 este indigo.')
+elif numar_rand == 7:
+ print('Culoarea cu numarul de rand 7 este violet.')
+elif numar_rand >7:
+ print('Error')
